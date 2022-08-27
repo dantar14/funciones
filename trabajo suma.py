a0f43d92681cb5f2efbcf27eacd378a1067ae245
dantar14 <111466337+dantar14@users.noreply.github.com>
2022-08-27 15:35:45 -0500

    Add files via upload
    En este código definió una función en la que te imprimiera un mensaje de bienvenida,un nombre y tu edad

---
+++ trabajo suma.py
@@ -0,0 +1,9 @@
+def mensaje(msg:str,nombre:str):
+    print(msg,nombre)
+def mensaje_edad(nombre:str, edad:int):
+    print("Hola",nombre,"tienes",edad,"años")
+
+if __name__== "__main__":
+    mensaje("Buenos dias","Dante")
+    mensaje_edad("Dante", 19)
+
